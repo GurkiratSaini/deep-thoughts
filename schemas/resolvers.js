6ae77616f60f2c719f9d7b35332c7463ca5dc2b1
Gurkirat Saini <78436028+GurkiratSaini@users.noreply.github.com>
2021-07-12 15:06:16 -0400

    added typedef and resolver for auth

--- schemas/resolvers.js
+++ schemas/resolvers.js
@@ -1,5 +1,6 @@
 const { User, Thought } = require('../models');
 const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
@@ -24,13 +25,26 @@ const resolvers = {
         .select('-__v -password')
         .populate('friends')
         .populate('thoughts');
+      },
+
+      me: async (parent, args, context) => {
+        if (context.user){
+        const userData = await User.findOne({})
+        .select('-__v -password')
+        .populate('thoughts')
+        .populate('friends');
+
+        return userData;
+      }
+      throw new AuthenticationError('Not logged in');
       }
     },
     Mutation: {
       addUser: async (parent, args) => {
         const user = await User.create(args);
-      
-        return user;
+        const token = signToken(user);
+
+        return {token, user};
       },
       login: async (parent, { email, password }) => {
         const user = await User.findOne({ email });
@@ -45,7 +59,9 @@ const resolvers = {
           throw new AuthenticationError('Incorrect credentials');
         }
 
-        return user;
+        const token = signToken(user);
+
+        return {token, user};
       }
     }
   };
